acb7d8e5fd11f006cb3fdb94948d20bc4795e7d1
VARADHARAJAN4 <85033135+VARADHARAJAN4@users.noreply.github.com>
2022-05-12 08:19:16 +0530

    step 1 updated

--- example1.java
+++ example1.java
@@ -3,6 +3,7 @@ public class example1 {
 	public static void main(String args[])
 	{	  
 		  System.out.println("Hello Learner");  
-		 System.out.println("Hello JFS developer");
+		 System.out.println("Hello JFS developers111111");
+		 System.out.print("Good to know");
 	}
 }
