a29f16f21ca65d9bc9265b56a8a3f123a214fdcd
VARADHARAJAN4 <85033135+VARADHARAJAN4@users.noreply.github.com>
2022-05-12 07:23:54 +0530

    Create example1.java

---
+++ example1.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class example1 {
+	public static void main(String args[])
+	{	  
+		  System.out.println("Hello Learner");  
+		 System.out.println("Hello JFS developer");
+	}
+}
